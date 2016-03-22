ae89e98a0c999f9c16ed562c54704d820d140144
chris84948 <chris84948@gmail.com>
2016-03-22 08:54:09 -0500

    Custom window working
    Title bar now acts like a regular window

---
+++ NotedUI/AttachedBehaviors/ButtonBehaviors.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace NotedUI.AttachedBehaviors
+{
+    public class ButtonBehaviors : DependencyObject
+    {
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register("Icon", typeof(Visual), typeof(ButtonBehaviors), new PropertyMetadata(null));
+
+        public static readonly DependencyProperty IconSizeProperty =
+            DependencyProperty.Register("IconSize", typeof(double), typeof(ButtonBehaviors),
+                new FrameworkPropertyMetadata(0.0));
+
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(ButtonBehaviors), new PropertyMetadata(""));
+
+        public static Visual GetIcon(DependencyObject obj)
+        {
+            return (Visual)obj.GetValue(IconProperty);
+        }
+
+        public static void SetIcon(DependencyObject obj, Visual value)
+        {
+            obj.SetValue(IconProperty, value);
+        }
+
+        public static double GetIconSize(DependencyObject obj)
+        {
+            return (double)obj.GetValue(IconSizeProperty);
+        }
+
+        public static void SetIconSize(DependencyObject obj, double value)
+        {
+            obj.SetValue(IconSizeProperty, value);
+        }
+
+        public static string GetText(DependencyObject obj)
+        {
+            return (string)obj.GetValue(TextProperty);
+        }
+
+        public static void SetText(DependencyObject obj, string value)
+        {
+            obj.SetValue(TextProperty, value);
+        }
+    }
+}
---
+++ NotedUI/Themes/NotedWindow.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace NotedUI.Themes
+{
+    public class NotedWindow : Window
+    {
+        static NotedWindow()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(NotedWindow), new FrameworkPropertyMetadata(typeof(NotedWindow)));
+        }
+
+        public NotedWindow()
+        {
+            this.CommandBindings.Add(new CommandBinding(SystemCommands.CloseWindowCommand, OnCloseWindow));
+            this.CommandBindings.Add(new CommandBinding(SystemCommands.MaximizeWindowCommand, OnMaximizeWindow, OnCanResizeWindow));
+            this.CommandBindings.Add(new CommandBinding(SystemCommands.RestoreWindowCommand, OnRestoreWindow, OnCanResizeWindow));
+            this.CommandBindings.Add(new CommandBinding(SystemCommands.MinimizeWindowCommand, OnMinimizeWindow, OnCanMinimizeWindow));
+
+            this.MouseDown += (s, e) =>
+            {
+                if (e.ChangedButton == MouseButton.Left)
+                {
+                    this.DragMove();
+                }
+            };
+
+            this.MouseDoubleClick += (s, e) =>
+            {
+                if (e.ChangedButton == MouseButton.Left)
+                {
+                    if (WindowState == WindowState.Maximized)
+                        WindowState = WindowState.Normal;
+                    else
+                        WindowState = WindowState.Maximized;
+                }
+            };
+        }
+
+        private void OnCanMinimizeWindow(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = this.ResizeMode != ResizeMode.NoResize;
+        }
+
+        private void OnCanResizeWindow(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = this.ResizeMode == ResizeMode.CanResize || this.ResizeMode == ResizeMode.CanResizeWithGrip;
+        }
+
+        private void OnCloseWindow(object sender, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.CloseWindow(this);
+        }
+
+        private void OnMaximizeWindow(object sender, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.MaximizeWindow(this);
+        }
+
+        private void OnRestoreWindow(object sender, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.RestoreWindow(this);
+        }
+
+        private void OnMinimizeWindow(object sender, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.MinimizeWindow(this);
+        }
+    }
+}
