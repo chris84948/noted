f2a3debc6719ce3f522767e90aa5f17decdf0f51
chris84948 <chris84948@gmail.com>
2016-03-24 22:46:32 -0500

    Finished up FormatMenu.xaml
    Added all RelayCommands into FormatMenu.
    Added FormatCommands class to handle all formatting in one place.

---
+++ NotedUI/UI/FormatCommands.cs
@@ -0,0 +1,193 @@
+﻿using JustMVVM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace NotedUI.UI
+{
+    public class FormatCommands : MVVMBase
+    {
+        public ICommand Header1Command { get { return new RelayCommand<TextBox>(Header1Exec, CanHeader1Exec); } }
+        public ICommand Header2Command { get { return new RelayCommand<TextBox>(Header2Exec, CanHeader2Exec); } }
+        public ICommand Header3Command { get { return new RelayCommand<TextBox>(Header3Exec, CanHeader3Exec); } }
+        public ICommand Header4Command { get { return new RelayCommand<TextBox>(Header4Exec, CanHeader4Exec); } }
+        public ICommand Header5Command { get { return new RelayCommand<TextBox>(Header5Exec, CanHeader5Exec); } }
+        public ICommand Header6Command { get { return new RelayCommand<TextBox>(Header6Exec, CanHeader6Exec); } }
+        public ICommand BoldCommand { get { return new RelayCommand<TextBox>(BoldExec, CanBoldExec); } }
+        public ICommand ItalicCommand { get { return new RelayCommand<TextBox>(ItalicExec, CanItalicExec); } }
+        public ICommand StrikethroughCommand { get { return new RelayCommand<TextBox>(StrikethroughExec, CanStrikethroughExec); } }
+        public ICommand QuotesCommand { get { return new RelayCommand<TextBox>(QuotesExec, CanQuotesExec); } }
+        public ICommand CodeCommand { get { return new RelayCommand<TextBox>(CodeExec, CanCodeExec); } }
+        public ICommand BulletPointCommand { get { return new RelayCommand<TextBox>(BulletPointExec, CanBulletPointExec); } }
+        public ICommand ListCommand { get { return new RelayCommand<TextBox>(ListExec, CanListExec); } }
+        public ICommand ImageCommand { get { return new RelayCommand<TextBox>(ImageExec, CanImageExec); } }
+        public ICommand LinkCommand { get { return new RelayCommand<TextBox>(LinkExec, CanLinkExec); } }
+        public ICommand HorizontalRuleCommand { get { return new RelayCommand<TextBox>(HorizontalRuleExec, CanHorizontalRuleExec); } }
+
+        public bool CanHeader1Exec(TextBox contentTextBox)
+        {
+            return true;
+        }
+
+        public void Header1Exec(TextBox contentTextBox)
+        {
+
+        }
+
+        public bool CanHeader2Exec(TextBox contentTextBox)
+        {
+            return true;
+        }
+
+        public void Header2Exec(TextBox contentTextBox)
+        {
+
+        }
+
+        public bool CanHeader3Exec(TextBox contentTextBox)
+        {
+            return true;
+        }
+
+        public void Header3Exec(TextBox contentTextBox)
+        {
+
+        }
+
+        public bool CanHeader4Exec(TextBox contentTextBox)
+        {
+            return true;
+        }
+
+        public void Header4Exec(TextBox contentTextBox)
+        {
+
+        }
+
+        public bool CanHeader5Exec(TextBox contentTextBox)
+        {
+            return true;
+        }
+
+        public void Header5Exec(TextBox contentTextBox)
+        {
+
+        }
+
+        public bool CanHeader6Exec(TextBox contentTextBox)
+        {
+            return true;
+        }
+
+        public void Header6Exec(TextBox contentTextBox)
+        {
+
+        }
+
+        public bool CanBoldExec(TextBox contentTextBox)
+        {
+            return true;
+        }
+
+        public void BoldExec(TextBox contentTextBox)
+        {
+
+        }
+
+        public bool CanItalicExec(TextBox contentTextBox)
+        {
+            return true;
+        }
+
+        public void ItalicExec(TextBox contentTextBox)
+        {
+
+        }
+
+        public bool CanStrikethroughExec(TextBox contentTextBox)
+        {
+            return true;
+        }
+
+        public void StrikethroughExec(TextBox contentTextBox)
+        {
+
+        }
+
+        public bool CanQuotesExec(TextBox contentTextBox)
+        {
+            return true;
+        }
+
+        public void QuotesExec(TextBox contentTextBox)
+        {
+
+        }
+
+        public bool CanCodeExec(TextBox contentTextBox)
+        {
+            return true;
+        }
+
+        public void CodeExec(TextBox contentTextBox)
+        {
+
+        }
+
+        public bool CanBulletPointExec(TextBox contentTextBox)
+        {
+            return true;
+        }
+
+        public void BulletPointExec(TextBox contentTextBox)
+        {
+
+        }
+
+        public bool CanListExec(TextBox contentTextBox)
+        {
+            return true;
+        }
+
+        public void ListExec(TextBox contentTextBox)
+        {
+
+        }
+
+        public bool CanImageExec(TextBox contentTextBox)
+        {
+            return true;
+        }
+
+        public void ImageExec(TextBox contentTextBox)
+        {
+
+        }
+
+        public bool CanLinkExec(TextBox contentTextBox)
+        {
+            return true;
+        }
+
+        public void LinkExec(TextBox contentTextBox)
+        {
+
+        }
+
+        public bool CanHorizontalRuleExec(TextBox contentTextBox)
+        {
+            return true;
+        }
+
+        public void HorizontalRuleExec(TextBox contentTextBox)
+        {
+
+        }
+
+
+    }
+}
--- NotedUI/UI/FormatMenu.xaml.cs
+++ NotedUI/UI/FormatMenu.xaml.cs
@@ -21,13 +21,21 @@ namespace NotedUI.UI
     /// </summary>
     public partial class FormatMenu : UserControl
     {
-        // TODO this is just a placeholder for now. Likely I'll be passing a textbox
-        // But I may want to change it, so for now it's just an object
-        private object _commandParameter;
+        public static readonly DependencyProperty TextBoxProperty =
+            DependencyProperty.Register("TextBox", 
+                                        typeof(TextBox), 
+                                        typeof(FormatMenu),
+                                        new FrameworkPropertyMetadata(null));
 
-        public static readonly DependencyProperty Header1CommandProperty = 
-            DependencyProperty.Register("Header1Command", 
-                                        typeof(ICommand), 
+        public TextBox TextBox
+        {
+            get { return (TextBox)GetValue(TextBoxProperty); }
+            set { SetValue(TextBoxProperty, value); }
+        }
+
+        public static readonly DependencyProperty Header1CommandProperty =
+            DependencyProperty.Register("Header1Command",
+                                        typeof(ICommand),
                                         typeof(FormatMenu),
                                         new PropertyMetadata((ICommand)null));
 
@@ -38,6 +46,200 @@ namespace NotedUI.UI
             set { SetValue(Header1CommandProperty, value); }
         }
 
+        public static readonly DependencyProperty Header2CommandProperty =
+            DependencyProperty.Register("Header2Command",
+                                        typeof(ICommand),
+                                        typeof(FormatMenu),
+                                        new PropertyMetadata((ICommand)null));
+
+        [TypeConverter(typeof(CommandConverter))]
+        public ICommand Header2Command
+        {
+            get { return (ICommand)GetValue(Header2CommandProperty); }
+            set { SetValue(Header2CommandProperty, value); }
+        }
+
+        public static readonly DependencyProperty Header3CommandProperty =
+            DependencyProperty.Register("Header3Command",
+                                        typeof(ICommand),
+                                        typeof(FormatMenu),
+                                        new PropertyMetadata((ICommand)null));
+
+        [TypeConverter(typeof(CommandConverter))]
+        public ICommand Header3Command
+        {
+            get { return (ICommand)GetValue(Header3CommandProperty); }
+            set { SetValue(Header3CommandProperty, value); }
+        }
+
+        public static readonly DependencyProperty Header4CommandProperty =
+            DependencyProperty.Register("Header4Command",
+                                        typeof(ICommand),
+                                        typeof(FormatMenu),
+                                        new PropertyMetadata((ICommand)null));
+
+        [TypeConverter(typeof(CommandConverter))]
+        public ICommand Header4Command
+        {
+            get { return (ICommand)GetValue(Header4CommandProperty); }
+            set { SetValue(Header4CommandProperty, value); }
+        }
+
+        public static readonly DependencyProperty Header5CommandProperty =
+            DependencyProperty.Register("Header5Command",
+                                        typeof(ICommand),
+                                        typeof(FormatMenu),
+                                        new PropertyMetadata((ICommand)null));
+
+        [TypeConverter(typeof(CommandConverter))]
+        public ICommand Header5Command
+        {
+            get { return (ICommand)GetValue(Header5CommandProperty); }
+            set { SetValue(Header5CommandProperty, value); }
+        }
+
+        public static readonly DependencyProperty Header6CommandProperty =
+            DependencyProperty.Register("Header6Command",
+                                        typeof(ICommand),
+                                        typeof(FormatMenu),
+                                        new PropertyMetadata((ICommand)null));
+
+        [TypeConverter(typeof(CommandConverter))]
+        public ICommand Header6Command
+        {
+            get { return (ICommand)GetValue(Header6CommandProperty); }
+            set { SetValue(Header6CommandProperty, value); }
+        }
+
+        public static readonly DependencyProperty BoldCommandProperty =
+            DependencyProperty.Register("BoldCommand",
+                                        typeof(ICommand),
+                                        typeof(FormatMenu),
+                                        new PropertyMetadata((ICommand)null));
+
+        [TypeConverter(typeof(CommandConverter))]
+        public ICommand BoldCommand
+        {
+            get { return (ICommand)GetValue(BoldCommandProperty); }
+            set { SetValue(BoldCommandProperty, value); }
+        }
+
+        public static readonly DependencyProperty ItalicCommandProperty =
+            DependencyProperty.Register("ItalicCommand",
+                                        typeof(ICommand),
+                                        typeof(FormatMenu),
+                                        new PropertyMetadata((ICommand)null));
+
+        [TypeConverter(typeof(CommandConverter))]
+        public ICommand ItalicCommand
+        {
+            get { return (ICommand)GetValue(ItalicCommandProperty); }
+            set { SetValue(ItalicCommandProperty, value); }
+        }
+
+        public static readonly DependencyProperty StrikethroughCommandProperty =
+            DependencyProperty.Register("StrikethroughCommand",
+                                        typeof(ICommand),
+                                        typeof(FormatMenu),
+                                        new PropertyMetadata((ICommand)null));
+
+        [TypeConverter(typeof(CommandConverter))]
+        public ICommand StrikethroughCommand
+        {
+            get { return (ICommand)GetValue(StrikethroughCommandProperty); }
+            set { SetValue(StrikethroughCommandProperty, value); }
+        }
+
+        public static readonly DependencyProperty QuotesCommandProperty =
+            DependencyProperty.Register("QuotesCommand",
+                                        typeof(ICommand),
+                                        typeof(FormatMenu),
+                                        new PropertyMetadata((ICommand)null));
+
+        [TypeConverter(typeof(CommandConverter))]
+        public ICommand QuotesCommand
+        {
+            get { return (ICommand)GetValue(QuotesCommandProperty); }
+            set { SetValue(QuotesCommandProperty, value); }
+        }
+
+        public static readonly DependencyProperty CodeCommandProperty =
+            DependencyProperty.Register("CodeCommand",
+                                        typeof(ICommand),
+                                        typeof(FormatMenu),
+                                        new PropertyMetadata((ICommand)null));
+
+        [TypeConverter(typeof(CommandConverter))]
+        public ICommand CodeCommand
+        {
+            get { return (ICommand)GetValue(CodeCommandProperty); }
+            set { SetValue(CodeCommandProperty, value); }
+        }
+
+        public static readonly DependencyProperty BulletPointCommandProperty =
+            DependencyProperty.Register("BulletPointCommand",
+                                        typeof(ICommand),
+                                        typeof(FormatMenu),
+                                        new PropertyMetadata((ICommand)null));
+
+        [TypeConverter(typeof(CommandConverter))]
+        public ICommand BulletPointCommand
+        {
+            get { return (ICommand)GetValue(BulletPointCommandProperty); }
+            set { SetValue(BulletPointCommandProperty, value); }
+        }
+
+        public static readonly DependencyProperty ListCommandProperty =
+            DependencyProperty.Register("ListCommand",
+                                        typeof(ICommand),
+                                        typeof(FormatMenu),
+                                        new PropertyMetadata((ICommand)null));
+
+        [TypeConverter(typeof(CommandConverter))]
+        public ICommand ListCommand
+        {
+            get { return (ICommand)GetValue(ListCommandProperty); }
+            set { SetValue(ListCommandProperty, value); }
+        }
+
+        public static readonly DependencyProperty ImageCommandProperty =
+            DependencyProperty.Register("ImageCommand",
+                                        typeof(ICommand),
+                                        typeof(FormatMenu),
+                                        new PropertyMetadata((ICommand)null));
+
+        [TypeConverter(typeof(CommandConverter))]
+        public ICommand ImageCommand
+        {
+            get { return (ICommand)GetValue(ImageCommandProperty); }
+            set { SetValue(ImageCommandProperty, value); }
+        }
+
+        public static readonly DependencyProperty LinkCommandProperty =
+            DependencyProperty.Register("LinkCommand",
+                                        typeof(ICommand),
+                                        typeof(FormatMenu),
+                                        new PropertyMetadata((ICommand)null));
+
+        [TypeConverter(typeof(CommandConverter))]
+        public ICommand LinkCommand
+        {
+            get { return (ICommand)GetValue(LinkCommandProperty); }
+            set { SetValue(LinkCommandProperty, value); }
+        }
+
+        public static readonly DependencyProperty HorizontalLineCommandProperty =
+            DependencyProperty.Register("HorizontalLineCommand",
+                                        typeof(ICommand),
+                                        typeof(FormatMenu),
+                                        new PropertyMetadata((ICommand)null));
+
+        [TypeConverter(typeof(CommandConverter))]
+        public ICommand HorizontalLineCommand
+        {
+            get { return (ICommand)GetValue(HorizontalLineCommandProperty); }
+            set { SetValue(HorizontalLineCommandProperty, value); }
+        }
 
 
         public FormatMenu()
@@ -47,77 +249,82 @@ namespace NotedUI.UI
 
         private void Header1_Click(object sender, RoutedEventArgs e)
         {
-            Header1Command?.Execute(_commandParameter);
+            Header1Command?.Execute(TextBox);
         }
 
         private void Header2_Click(object sender, RoutedEventArgs e)
         {
-
+            Header2Command?.Execute(TextBox);
         }
 
         private void Header3_Click(object sender, RoutedEventArgs e)
         {
-
+            Header3Command?.Execute(TextBox);
         }
 
         private void Header4_Click(object sender, RoutedEventArgs e)
         {
-
+            Header4Command?.Execute(TextBox);
         }
 
         private void Header5_Click(object sender, RoutedEventArgs e)
         {
-
+            Header5Command?.Execute(TextBox);
         }
 
         private void Header6_Click(object sender, RoutedEventArgs e)
         {
-
+            Header6Command?.Execute(TextBox);
         }
 
         private void Bold_Click(object sender, RoutedEventArgs e)
         {
-
+            BoldCommand?.Execute(TextBox);
         }
 
         private void Italic_Click(object sender, RoutedEventArgs e)
         {
-
+            ItalicCommand?.Execute(TextBox);
         }
 
         private void Strikethrough_Click(object sender, RoutedEventArgs e)
         {
-
+            StrikethroughCommand?.Execute(TextBox);
         }
 
         private void Quote_Click(object sender, RoutedEventArgs e)
         {
-
+            QuotesCommand?.Execute(TextBox);
         }
 
         private void Code_Click(object sender, RoutedEventArgs e)
         {
-
+            CodeCommand?.Execute(TextBox);
         }
 
         private void BulletPoint_Click(object sender, RoutedEventArgs e)
         {
-
+            BulletPointCommand?.Execute(TextBox);
         }
 
         private void List_Click(object sender, RoutedEventArgs e)
         {
-
+            ListCommand?.Execute(TextBox);
         }
 
         private void Image_Click(object sender, RoutedEventArgs e)
         {
-
+            ImageCommand?.Execute(TextBox);
         }
 
         private void Link_Click(object sender, RoutedEventArgs e)
         {
+            LinkCommand?.Execute(TextBox);
+        }
 
+        private void HorizontalLine_Click(object sender, RoutedEventArgs e)
+        {
+            HorizontalLineCommand?.Execute(TextBox);
         }
     }
 }
