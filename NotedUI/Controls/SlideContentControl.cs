7a0844f7a250b52410d2d0b0438b2a84ecfd5482
chris84948 <chris84948@gmail.com>
2016-04-03 22:18:42 -0500

    Added SlideContentControl class

---
+++ NotedUI/Controls/SlideContentControl.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NotedUI.Controls
+{
+    public class SlideContentControl : ContentControl
+    {
+        static SlideContentControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(SlideContentControl), new FrameworkPropertyMetadata(typeof(SlideContentControl)));
+
+            ContentProperty.OverrideMetadata(typeof(SlideContentControl), new FrameworkPropertyMetadata(null, ContentHasChanged));
+        }
+
+        private static object ContentHasChanged(DependencyObject d, object baseValue)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
