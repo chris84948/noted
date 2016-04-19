1d04ed026791bbf8d4b86e01fd046128ea409ba3
chris84948 <chris84948@gmail.com>
2016-04-18 23:55:32 -0500

    Added AvalonEdit and MarkdownSharp
    AvalonEdit textbox is working.
    Markdownsharp markdown preview is working.

---
+++ NotedUI/AttachedBehaviors/AvalonEditBehavior.cs
@@ -0,0 +1,60 @@
+﻿using ICSharpCode.AvalonEdit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Interactivity;
+
+namespace NotedUI.AttachedBehaviors
+{
+    public static class AvalonEditBehaviour
+    {
+        public static readonly DependencyProperty BindToTextProperty =
+            DependencyProperty.RegisterAttached("BindToText",
+                                                typeof(bool),
+                                                typeof(AvalonEditBehaviour),
+                                                new PropertyMetadata(false, new PropertyChangedCallback(BindToTextChanged)));
+
+        public static bool GetBindToText(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(BindToTextProperty);
+        }
+
+        public static void SetBindToText(DependencyObject obj, bool value)
+        {
+            obj.SetValue(BindToTextProperty, value);
+        }
+
+        private static void BindToTextChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            TextEditor editor = (TextEditor)d;
+            bool newVal = (bool)e.NewValue;
+
+            if (!newVal)
+                return;
+
+            editor.TextChanged += (sender, args) =>
+            {
+                SetBindableText(d, editor.Text);
+            };
+        }
+
+        public static readonly DependencyProperty BindableTextProperty =
+            DependencyProperty.RegisterAttached("BindableText",
+                                                typeof(string),
+                                                typeof(AvalonEditBehaviour),
+                                                new PropertyMetadata(null));
+
+        public static string GetBindableText(DependencyObject obj)
+        {
+            return (string)obj.GetValue(BindableTextProperty);
+        }
+
+        public static void SetBindableText(DependencyObject obj, string value)
+        {
+            obj.SetValue(BindableTextProperty, value);
+        }
+    }
+}
---
+++ NotedUI/Converters/MarkdownConverter.cs
@@ -0,0 +1,30 @@
+﻿using MarkdownSharp;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace NotedUI.Converters
+{
+    [ValueConversion(typeof(string), typeof(string))]
+    public class MarkdownConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is string))
+                return "";
+
+            Markdown markdown = new Markdown();
+            return markdown.Transform(value.ToString());
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
