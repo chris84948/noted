74c5ec8c5619ee6e52140a6c6020c54077afd977
chris84948 <chris84948@gmail.com>
2017-02-24 16:26:05 -0600

    Added About screen to settings

---
+++ NotedUI/UI/ViewModels/SettingsAboutViewModel.cs
@@ -0,0 +1,13 @@
+﻿using JustMVVM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotedUI.UI.ViewModels
+{
+    public class SettingsAboutViewModel : MVVMBase
+    {
+    }
+}
--- NotedUI/UI/ViewModels/SettingsViewModel.cs
+++ NotedUI/UI/ViewModels/SettingsViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using JustMVVM;
 using NotedUI.Controls;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
@@ -8,15 +10,64 @@ namespace NotedUI.UI.ViewModels
 {
     class SettingsViewModel : MVVMBase, IScreen
     {
-        public event Action<IScreen, eTransitionType> ChangeScreen;
-
         public ICommand CloseCommand { get { return new RelayCommand(CloseExec); } }
 
+        private List<string> _settingsNameCollection;
+        public List<string> SettingsNameCollection
+        {
+            get { return _settingsNameCollection; }
+            set
+            {
+                _settingsNameCollection = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _selectedSettingsName;
+        public string SelectedSettingsName
+        {
+            get { return _selectedSettingsName; }
+            set
+            {
+                _selectedSettingsName = value;
+                OnPropertyChanged();
+
+                ChangeView(_selectedSettingsName);
+            }
+        }
+
+        private MVVMBase _settingsView;
+        public MVVMBase SettingsView
+        {
+            get { return _settingsView; }
+            set
+            {
+                _settingsView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event Action<IScreen, eTransitionType> ChangeScreen;
+
         private HomeViewModel _homeVM;
+        private Dictionary<string, MVVMBase> _settingsScreens;
 
         public SettingsViewModel(HomeViewModel homeVM)
         {
             _homeVM = homeVM;
+
+            _settingsScreens = new Dictionary<string, MVVMBase>()
+            {
+                { "ABOUT", new SettingsAboutViewModel() },
+            };
+
+            SettingsNameCollection = _settingsScreens.Keys.ToList();
+        }
+
+        private void ChangeView(string settingsName)
+        {
+            if (_settingsScreens.ContainsKey(settingsName))
+                SettingsView = _settingsScreens[settingsName];
         }
 
         private void CloseExec()
